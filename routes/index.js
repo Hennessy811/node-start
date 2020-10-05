daec54325208d7a43493d233b14902320c98d4b8
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-10-05 14:42:58 +0300

    basic changes applied

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "My new app" });
+  res.json({
+    message: "Holy shit! Its alive!",
+  });
 });
 
 module.exports = router;
